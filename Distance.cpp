61c6bfb810c9b508b2760f4478253a62ab146e3e
netanel <netanel7227@gmail.com>
2022-12-14 16:56:45 +0200

    Added comments

--- Distance.cpp
+++ Distance.cpp
@@ -4,4 +4,12 @@
 
 using namespace std;
 
+/**
+ * @brief An abstract distance class.
+ * 
+ * @param vec1 first vector
+ * @param vec2 second vector
+ * @param vectorSizes size of the vectors
+ * @return double the distance between them
+ */
 double Distance::getDistance(vector<double> vec1, vector<double> vec2, int vectorSizes) {return -1;};
--- KNN.cpp
+++ KNN.cpp
@@ -7,6 +7,10 @@
 #include <string>
 using namespace std;
 
+/**
+ * @brief A comparator class to compare vectors based on their distance from a vector.
+ * Both the vector and the distance function are given.
+ */
 class vectorCmp {
 public:
     vector<double> vec;
@@ -24,16 +28,28 @@ public:
     }
 };
 
+/**
+ * @brief A method to return a list of the k closest neighbors to a given vector using a given distance method.
+ * 
+ * @param vector1 the vector to calculate distance from
+ * @param distance the distance method
+ * @param vecMap map of vectors to type
+ * @param k number of neighbors to returns
+ * @return list<vector<double>> list of k closesnt neighbors to the given vectors using the given distance method.
+ */
 list<vector<double>> KNN::knnList(vector<double> vector1, Distance* distance, map<vector<double>, string> vecMap, int k) {
     vectorCmp vc(vector1, distance);
     list<vector<double>> vecList;
+    //Push all the vectors from the map into the list
     for (auto v : vecMap) {
         vecList.push_back(v.first);
     }
+    //Sort the list
     vecList.sort(vc);
     list<vector<double>> KList;
     list<vector<double>>::iterator it;
     int i = 0;
+    //Return the K closest neighbors.
     for (i = 0; i < k; i++) {
         it = vecList.begin();
         KList.splice(KList.end(), vecList, it);
@@ -41,18 +57,29 @@ list<vector<double>> KNN::knnList(vector<double> vector1, Distance* distance, ma
     return KList;
 }
 
+/**
+ * @brief A method to return the type that appeared the most out of the k closest neighbors.
+ * 
+ * @param KDistanceList A list of the K closesnt neighbors
+ * @param vecMap A map of the vectors to their type.
+ * @return string the type that appeared the most out of the k closest neighbors.
+ */
 string KNN::findVectorType(list<vector<double>> KDistanceList, map<vector<double>, string> vecMap){
+    //A map of how many times each type appeared.
     map<string, int> vecType;
     map<string, int>::iterator typeIter;
 
+    //Initialize all the values in the map to 0.
     for (vector<double> v : KDistanceList) {
         vecType[vecMap[v]] = 0;
     }
 
+    //Update the number of times the type appeared for each vector in the list.
     for (vector<double> v : KDistanceList) {
         vecType[vecMap[v]] += 1;
     }
 
+    //Find the maximum of and return it.
     string maxType = vecType.begin()->first;
     for (typeIter = vecType.begin(); typeIter != vecType.end() ; typeIter++) {
         if (vecType[maxType] < typeIter->second){
